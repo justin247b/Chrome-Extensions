9ed9208ae0a9765186a75fe306dda90fab031bb9
justin247b <justin247b@gmail.com>
2023-02-05 20:07:13 -0500

    Page Highlighting Extension

---
+++ script.js
@@ -0,0 +1,14 @@
+var selectedText = '';
+
+document.addEventListener("mousedown", function(event) {
+  selectedText = window.getSelection().toString();
+});
+
+document.addEventListener("mouseup", function(event) {
+  if (selectedText) {
+    var highlightDiv = document.getElementById("Highlight");
+    highlightDiv.innerHTML = selectedText;
+  }
+});
+
+  
